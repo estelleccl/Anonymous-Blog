26c0e8cab81481c3d46998d9a40258405230c868
estelle <estelleccl@gmail.com>
2014-10-28 07:27:05 +0000

    added show all posts, particular post, new post, edit existing post

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,6 +1,23 @@
 get '/posts' do
   @posts_list = Post.all
+  @posts_list_desc = Post.order( "created_at DESC" ).limit(10)
   erb :index
+end
+
+get '/posts/:id' do
+  @post= Post.find(params[:id])
+  @posts_list_desc = Post.order( "created_at DESC" ).limit(10)
+  erb :'post/post'
+end
+
+post '/posts/:id' do
+  @post= Post.find(params[:id])
+  puts "Post id #{@post.id}"
+  @post[:author] = params[:author]
+  @post[:title] = params[:title]
+  @post[:content] = params[:content]
+  @post.save
+  redirect "/posts/#{params[:id]}"
 end
 
 get '/new_post' do
@@ -9,13 +26,23 @@ end
 
 post '/new_post' do
   @post = Post.create(title: params[:title], content: params[:content], author: params[:author])
-  puts params[:tags].inspect
-  @tag = Tag.create(tag_name: params[:tags])
+  temp_tag = params[:tags]
+  Tag.create(tag_name: params[:tags])
 #   PostTag.create(  )
   if !@post.new_record? #Returns true if this object hasn’t been saved yet
      redirect "/"
   else
      @error_msg = @post.errors.full_messages
      erb :newpost
+  end
+end
+
+get '/posts/:id/edit' do
+  @post = Post.find params[:id] 
+  if !@post.nil?
+    erb :'post/edit'
+  else
+    @error_msg = @post.errors.full_messages
+    erb :invalid
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :posttags
   has_many :posts, through: :posttags
+  validates_uniqueness_of :tag_name
   validates :tag_name, :presence => true
 end
